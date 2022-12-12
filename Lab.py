42450a0da76dacbc5348ab38ec3322158853dfbb
Vincemanio <119021720+Vincemanio@users.noreply.github.com>
2022-12-12 14:35:11 -0700

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -1,25 +1,21 @@
+
+#Laboratory class
 class Laboratory:
     name = ""
     cost = 0
     laboratory_list = []
-
     def __init__(self,name,cost):
         self.name = name
         self.cost = cost
         self.laboratory_list.append(self)
-
-
     # Formats the Laboratory object similar to the laboratories.txt file 
     def format_lab_info(self):
-        return self.name + "_" + str(self.cost)
-    
+        return self.name + "_" + str(self.cost)    
     #  Asks the user to enter lab name and cost and forms a Laboratory object
     def enter_laboratory_info(self):
         name = input( " Enter the name of the libratory: ")
         cost = int(input("Enter the cost of the labratory: "))
         self.laboratory_list.append(Laboratory(name,cost))
-        
-        
     #  Reads the laboratories.txt file and fills its contents in a list of Laboratory objects
     def read_laboratory_file(self):
         x = open("Laboratory_Cost.txt", "r")
@@ -28,9 +24,8 @@ class Laboratory:
             name, cost = line.split ('_')
             self.laboratory_list.append (Laboratory(name,cost))
             x.close()
-            
-
     # Displays the list of laboratories
+    # loops the lab list and prints each lab in the correct format.
     def display_labs_list(self):
         x = open("Laboratory_Cost.txt", "r")
         lines = x.readlines()
@@ -38,10 +33,6 @@ class Laboratory:
             name, cost = line.split('_')
             print("{:<20} {:<15}".format(name,cost))
             x.close()
-        # loop the lab list and print each lab in the correct format.
-        
-            
-
     #  Writes the list of labs into the file laboratories.txt
     def write_list_of_labs_to_file(self):
         x = open("laboratory_Cost.txt", "w")
@@ -49,10 +40,6 @@ class Laboratory:
         for lab in self.laboratory_list:
             x.write("\n" + lab.format_lab_info())
         x.close()
-        # loop the lab list
-        # call format_lab_info() for each lab and write it to the file
-        
-        
     #    Adds and writes the lab name to the file in the format of the data 
     #    that is in the file       
     def add_lab_to_file(self):
@@ -62,11 +49,7 @@ class Laboratory:
         x = open("Laboratory_Cost.txt", "a")
         x.write("\n" + name + "_" + str(cost))
         x.close()
-
-        # call enter_laboratory_info() to ask for new lab data
-        # call format_lab_info() to format new data
-        # write the new data to the file
-
+      # while loop, this is where the choices are made
     def laboratory_menu(self):
         while 1:
             print("Laboratory Menu")
